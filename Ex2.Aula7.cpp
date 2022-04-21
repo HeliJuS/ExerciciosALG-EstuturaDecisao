98bfd838e4951f4a60f587b910d284a20678febc
HeliJuS <103338394+HeliJuS@users.noreply.github.com>
2022-04-21 18:10:17 -0300

    Update Ex2.Aula7.cpp
    Elaborar um programa que calcule o reajuste de salário de um funcionário. Considere que o funcionário deve receber um reajuste de 15% caso seu salário seja menorque R$ 500,00. Se o salário for maior ou igual a R$500,00, mas menor ou igual a R$ 1000,00, seu reajuste será de 10%, e caso seja ainda maior que R$ 1000,00, oreajuste deve ser de 5%.

--- Ex2.Aula7.cpp
+++ Ex2.Aula7.cpp
@@ -1,1 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main() {
+
+	float salario, reajuste;
+
+	printf("Informe o seu salario: ");
+	scanf_s("%f", &salario);
+
+	if (salario < 500)
+	{
+		reajuste = salario * 0.15;
+		printf("\nO salario teve reajuste de %.2f reais\n\n", reajuste);
+	}
+	else if (salario >= 500 && salario <= 1000)
+	{
+		reajuste = salario * 0.10;
+		printf("\nO salario teve reajuste de %.2f reais\n\n", reajuste);
+	}
+	else {
+		reajuste = salario * 0.5;
+		printf("\nO salario teve reajuste de %.2f reais\n\n", reajuste);
+	}
+
+	system("PAUSE");
+	return(0);
+}
